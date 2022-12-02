b1d180ed9f0c75fb131b32eae92432884bd40427
Praneet Bang <praneet.bang@sjsu.edu>
2022-12-02 13:55:11 -0800

    Add logic for checking diagonals

--- src/main/CheckForDiagonalsHandler.java
+++ src/main/CheckForDiagonalsHandler.java
@@ -3,7 +3,22 @@ public class CheckForDiagonalsHandler implements Handler {
 
 	public boolean handleRequest( SimplePane p, char token ) {
         boolean result = false;
-        return result;
+        var c = p.getCells();
+        if ((c[0][0].getToken() == token && c[1][1].getToken() == token && c[2][2].getToken() == token) ||
+        (c[0][2].getToken() == token && c[1][1].getToken() == token && c[2][0].getToken() == token)) {
+            result = true;
+        }
+        if ( result )
+        {
+            return result;
+        }
+        else
+        {
+            if ( successor != null )
+                return successor.handleRequest(p, token);
+            else
+                return result;
+        }
 	}
 
 	public void setSuccessor(Handler next) {
