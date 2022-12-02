664b2eac76f7390f61053aa1609288304c1d0e44
Praneet Bang <praneet.bang@sjsu.edu>
2022-12-02 13:54:51 -0800

    Add logic for checking columns

--- src/main/CheckForColumnsHandler.java
+++ src/main/CheckForColumnsHandler.java
@@ -3,7 +3,24 @@ public class CheckForColumnsHandler implements Handler {
 
 	public boolean handleRequest( SimplePane p, char token ) {
         boolean result = false;
-        return result;
+        var c = p.getCells();
+        for (int j = 0; j < 3; j++) {
+			if (c[0][j].getToken() == token && c[1][j].getToken() == token && c[2][j].getToken() == token) {
+				result = true;
+                break;
+			}
+        }
+        if ( result )
+        {
+            return result;
+        }
+        else
+        {
+            if ( successor != null )
+                return successor.handleRequest(p, token);
+            else
+                return result;
+        }
 	}
 
 	public void setSuccessor(Handler next) {
