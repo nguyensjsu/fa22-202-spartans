56ce9b90f0fcd0c05e33ac88790a1dbb1a7647fa
Praneet Bang <praneet.bang@sjsu.edu>
2022-12-02 13:52:42 -0800

    Create CheckForRowsHandler.java

---
+++ src/main/CheckForRowsHandler.java
@@ -0,0 +1,12 @@
+public class CheckForRowsHandler implements Handler {
+    private Handler successor = null;
+
+	public boolean handleRequest( SimplePane p, char token ) {
+        boolean result = false;
+        return result;
+	}
+
+	public void setSuccessor(Handler next) {
+        this.successor = next ;
+	}
+}
