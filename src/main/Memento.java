67de78b80b5a2f68b747f2b0c42465327aed2e1e
Aditya Kopalli <kopalliaditya96@gmail.com>
2022-12-02 00:15:51 -0800

    Adding classes to state management

--- src/main/Memento.java
+++ src/main/Memento.java
@@ -2,4 +2,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Memento {
+	private char shape;
+	private int row;
+	private int column;
+}
+
+class Originator {
+	private char shape;
+	private int row;
+	private int column;
+}
+
+class CareTaker {
 }
