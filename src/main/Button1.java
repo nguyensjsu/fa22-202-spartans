7b94cbf30bec00e03c15e793e952cfd0c3ba859b
VishwaTejendra Pernapati <vishwatejendra.pernapati@sjsu.edu>
2022-12-01 21:39:42 -0800

    Create Button1.java

---
+++ src/main/Button1.java
@@ -0,0 +1,13 @@
+public class Button1 {
+    class NewButton extends CommandButton {
+        public NewButton(String text) {
+            super(text);
+            this.setText("New");
+        }
+
+        @Override
+        public void Execute() {
+            getP().newGame();
+        }
+    }
+}
