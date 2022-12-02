ca95c1546fcea28177e11d3c1b19985154258a92
Praneet Bang <praneet.bang@sjsu.edu>
2022-12-02 13:54:32 -0800

    Add logic for checking rows

--- src/main/CheckForRowsHandler.java
+++ src/main/CheckForRowsHandler.java
@@ -3,7 +3,24 @@ public class CheckForRowsHandler implements Handler {
 
 	public boolean handleRequest( SimplePane p, char token ) {
         boolean result = false;
-        return result;
+        var c = p.getCells();
+        for (int i = 0; i < 3; i++) {
+            if (c[i][0].getToken() == token && c[i][1].getToken() == token && c[i][2].getToken() == token) {
+                result = true;
+                break;
+            }
+		}
+        if ( result )
+        {
+            return result;
+        }
+        else
+        {
+            if ( successor != null )
+                return successor.handleRequest(p, token);
+            else
+                return result;
+        }
 	}
 
 	public void setSuccessor(Handler next) {
