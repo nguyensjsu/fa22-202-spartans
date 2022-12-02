90122e662482f52fae9068c8896f8301bf0cfbab
Aditya Kopalli <kopalliaditya96@gmail.com>
2022-12-02 00:34:36 -0800

    Originatior class changes

--- src/main/Memento.java
+++ src/main/Memento.java
@@ -5,13 +5,62 @@ public class Memento {
 	private char shape;
 	private int row;
 	private int column;
+
+	public Memento(char shape, int row, int column) {
+		this.shape = shape;
+		this.row = row;
+		this.column = column;
+	}
+
+	public char getShape() {
+		return this.shape;
+	}
+
+	public int getRow() {
+		return this.row;
+	}
+
+	public int getColumn() {
+		return this.column;
+	}
 }
 
 class Originator {
 	private char shape;
 	private int row;
 	private int column;
+
+	public void setState(char shape, int row, int column) {
+		this.shape = shape;
+		this.row = row;
+		this.column = column;
+	}
+
+	public Memento saveStateToMemento() {
+		return new Memento(this.shape, this.row, this.column);
+	}
+
+	public void getStateFromMemento(Memento memento) {
+		if (memento != null) {
+			this.shape = memento.getShape();
+			this.row = memento.getRow();
+			this.column = memento.getColumn();
+		}
+	}
+
+	public char getShape() {
+		return this.shape;
+	}
+
+	public int getRow() {
+		return this.row;
+	}
+
+	public int getColumn() {
+		return this.column;
+	}
 }
 
 class CareTaker {
+	
 }
